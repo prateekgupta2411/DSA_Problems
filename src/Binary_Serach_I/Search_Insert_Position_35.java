054c808a762568faaaf5a2e1818f332774f088ee
Prateek Gupta <68748725+prateekgupta2411@users.noreply.github.com>
2026-02-10 00:03:37 +0530

    easy BS

---
+++ src/Binary_Serach_I/Search_Insert_Position_35.java
@@ -0,0 +1,11 @@
+package Binary_Serach_I;
+
+public class Search_Insert_Position {
+    public static void main(String[] args){
+        int arr[] = {1,2,4,7}, x = 6;
+
+    }
+    public static int insertPosition(int arr[]){
+
+    }
+}
